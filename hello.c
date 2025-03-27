838a1f5deedf2e3caa5f87957c44594cd3b16534
Algohercules <adarshrreso@gmail.com>
2025-03-27 09:12:02 +0530

    variable rules

--- hello.c
+++ hello.c
@@ -10,11 +10,20 @@
 // variables -- the name of a memory location which stores some data
 
 
-int main(){
+// int main(){
 
-    int number = 25;
-    char star = "*";
-    int age = 22;
-    float pi = 3.14;
+//     int number = 25;
+//     char star = "*";
+//     int age = 22;
+//     float pi = 3.14;
 
-}
+// }
+
+// variables are vase sensitive 
+// 1st character is alphabet or _
+// no comma/ blanch space
+// no other symbol than _
+
+
+
+
